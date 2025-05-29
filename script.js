2a1d07b83ec52032cdcda1bcc6295a0f601af6c4
tothmatyas <tothmatyas621@gmail.com>
2025-05-29 09:39:19 +0200

    nem lesz megdöntve xd

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ function drawGuitar(){
            tableWrapper.appendChild(fretDiv)
         }
     }
+     for(let sor = 1; sor<=5; sor++){
+        for(let oszlop = 1; oszlop <=1; oszlop++){
+           let hitDiv = document.createElement("div")
+           hitDiv.className = "hit"
+           tableWrapper.appendChild(hitDiv)
+        }
+    }
 
     startBtn.disabled = true
 }
